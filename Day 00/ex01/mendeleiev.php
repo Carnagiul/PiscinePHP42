6d0cf074bcee7550a8017f8c0a750d5bbb375441
Carnagiul <pierrequeruel@gmail.com>
2018-05-26 14:25:11 +0200

    Init php generation for mendeleiv
    php generator mendeleiv

---
+++ Day 00/ex01/mendeleiev.php
@@ -0,0 +1,24 @@
+<?php
+
+array(
+	array("H", "Hydrogène", 1, 1, "green", "https://fr.wikipedia.org/wiki/Hydrogène", "1-1"),
+	array("He", "Helium", 2, 4, "light_blue", "https://fr.wikipedia.org/wiki/Helium", "1-18"),
+	array("Li", "Lithium", 3, 6, "red", "https://fr.wikipedia.org/wiki/Lithium", "2-1"),
+	array("Be", "Béryllium", 4, 9, "orange", "https://fr.wikipedia.org/wiki/Béryllium", "2-2"),
+	array("B", "Bore", 5, 10, "dark_green", "https://fr.wikipedia.org/wiki/Bore", "2-13"),
+	array("C", "Carbone", 6, 12, "green", "https://fr.wikipedia.org/wiki/Carbone", "2-14"),
+	array("N", "Azote", 7, 14, "green", "https://fr.wikipedia.org/wiki/Azote", "2-15"),
+	array("O", "Oxygène", 8, 15, "green", "https://fr.wikipedia.org/wiki/Oxygène", "2-16"),
+	array("F", "Fluor", 9, 18, "yellow", "https://fr.wikipedia.org/wiki/Fluor", "2-17"),
+	array("N", "Néon", 10, 20, "light_blue", "https://fr.wikipedia.org/wiki/Néon", "2-18"),
+	array("Na", "Sodium", 11, 22, "red", "https://fr.wikipedia.org/wiki/Sodium", "3-1"),
+	array("Mg", "Magnésium", 12, 24, "orange", "https://fr.wikipedia.org/wiki/Magnésium", "3-2"),
+	array("Al", "Aluminium", 13, 26, "grey", "https://fr.wikipedia.org/wiki/Aluminium", "3-13"),
+	array("Si", "Silicium", 14, 28, "dark_green", "https://fr.wikipedia.org/wiki/Silicium", "3-14"),
+	array("P", "Phosphore", 15, 30, "green", "https://fr.wikipedia.org/wiki/Phosphore", "3-15"),
+	array("S", "Souffre", 16, 32, "green", "https://fr.wikipedia.org/wiki/Souffre", "3-16"),
+	array("Cl", "Chlore", 17, 35, "yellow", "https://fr.wikipedia.org/wiki/Chlore", "3-17"),
+	array("Ar", "Argon", 18, 39, "light_blue", "https://fr.wikipedia.org/wiki/Argon", "3-18"),
+	array("K", "Potassium", 19, 39, "red", "https://fr.wikipedia.org/wiki/Potassium", "4-1"),
+	array("Ca", "Calcium", 20, 40, "orange", "https://fr.wikipedia.org/wiki/Calcium", "4-2"),
+}

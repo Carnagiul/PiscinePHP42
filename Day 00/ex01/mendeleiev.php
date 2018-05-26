da50c3357581a7b78e1eb6a9e7aee00610366cf4
Carnagiul <pierrequeruel@gmail.com>
2018-05-26 16:06:22 +0200

    Finish Mendeleiv
    Terminate Mendeleiev

--- Day 00/ex01/mendeleiev.php
+++ Day 00/ex01/mendeleiev.php
@@ -1,76 +1,168 @@
 <?php
 
+$array =
 array(
-	array("H", "Hydrogène", 1, 1, "green", "1"),
-	array("He", "Helium", 2, 4, "light_blue", "1"),
-	array("Li", "Lithium", 3, 6, "red", "2"),
-	array("Be", "Béryllium", 4, 9, "orange", "2"),
-	array("B", "Bore", 5, 10, "dark_green", "2"),
-	array("C", "Carbone", 6, 12, "green", "2"),
-	array("N", "Azote", 7, 14, "green", "2"),
-	array("O", "Oxygène", 8, 15, "green", "2"),
-	array("F", "Fluor", 9, 18, "yellow",  "2"),
-	array("N", "Néon", 10, 20, "light_blue", "2"),
-	array("Na", "Sodium", 11, 22, "red", "3"),
-	array("Mg", "Magnésium", 12, 24, "orange", "3"),
-	array("Al", "Aluminium", 13, 26, "grey", "3"),
-	array("Si", "Silicium", 14, 28, "dark_green", "3"),
-	array("P", "Phosphore", 15, 30, "green", "3"),
-	array("S", "Souffre", 16, 32, "green", "3"),
-	array("Cl", "Chlore", 17, 35, "yellow", "3"),
-	array("Ar", "Argon", 18, 39, "light_blue", "3"),
-	array("K", "Potassium", 19, 39, "red", "4"),
-	array("Ca", "Calcium", 20, 40, "orange", "4"),
-	array("Sc", "Scandium", 21, 44, "saumon", "4"),
-	array("Ti", "Titane", 22, 47, "saumon", "4"),
-	array("V", "Vanadium", 23, 51, "saumon", "4"),
-	array("Cr", "Chrome", 24, 52, "saumon", "4"),
-	array("Mn", "Manganèse", 25, 55, "saumon", "4"),
-	array("Fe", "Fer", 26, 56, "saumon", "4"),
-	array("Co", "Cobalt", 27, 59, "saumon", "4"),
-	array("Ni", "Nickel", 28, 58, "saumon", "4"),
-	array("Cu", "Cuivre", 29, 63, "saumon", "4"),
-	array("Zn", "Zinc", 30, 65, "grey", "4"),
-	array("Ga", "Gallium", 31, 69, "grey", "4"),
-	array("Ge", "Germanium", 32, 72, "dark_green", "4"),
-	array("As", "Arsenic", 33, 74, "grey", "4"),
-	array("Se", "Sélénium", 34, 78, "green", "4"),
-	array("Br", "Brome", 35, 79, "yellow", "4"),
-	array("Kr", "Krypton", 36, 83, "light_blue", "4"),
-	array("Rb", "Rubidium", 37, 85, "red", "5"),
-	array("Sr", "Strontium", 38, 87, "orange", "5"),
-	array("Y", "Yttrium", 39, 89, "saumon", "5"),
-	array("Zr", "Zirconium", 40, 91, "saumon", "5"),
-	array("Nb", "Niobium", 41, 93, "saumon", "5"),
-	array("Mo", "Molybdène", 42, 96, "saumon", "5"),
-	array("Tc", "Technétium", 43, 98, "saumon", "5"),
-	array("Ru", "Ruthénium", 44, 101, "saumon", "5"),
-	array("Rh", "Rhodium", 45, 103, "saumon", "5"),
-	array("Pd", "Palladium", 46, 106, "saumon", "5"),
-	array("Ag", "Argent", 47, 107, "saumon", "5"),
-	array("Cd", "Cadmium", 48, 112, "grey", "5"),
-	array("In", "Indium", 49, 114, "grey", "5"),
-	array("Sn", "Étain", 50, 118, "grey", "5"),
-	array("Sb", "Antimoine", 51, 121, "dark_green", "5"),
-	array("Te", "Tellure", 52, 127, "dark_green", "5"),
-	array("I", "Iode", 53, 126, "yellow", "5"),
-	array("Xe", "Xénon", 54, 131, "light_blue", "5"),
-	array("Cs", "Césium", 55, 133, "red", "6"),
-	array("Ba", "Baryum", 56, 137, "orange", "6"),
-	array("Lu", "Lutécium", 57, 138, "orange", "6"),
-	array("Hf", "Hafnium", 72, 178, "orange", "6"),
-	array("Ta", "Tantale ", 73, 181, "orange", "6"),
-	array("W", "Tungstène", 74, 184, "orange", "6"),
-	array("Re", "Rhénium", 75, 186, "orange", "6"),
-	array("Os", "Osmium", 76, 190, "orange", "6"),
-	array("Ir", "Iridium", 77, 192, "orange", "6"),
-	array("Pt", "Platine", 78, 195, "orange", "6"),
-	array("Au", "Or", 79, 196, "orange", "6"),
-	array("Hg", "Mercure", 80, 200, "grey", "6"),
-	array("Ti", "Thallium", 81, 204, "grey", "6"),
-	array("Pb", "Plomb", 82, 207, "grey", "6"),
-	array("Bi", "Bismuth", 83, 209, "grey", "6"),
-	array("Po", "Polonium", 84, 209, "grey", "6"),
-	array("At", "Astate", 85, 210, "dark_green", "6"),
-	array("Rn", "Radon", 86, 222, "light_blue", "6"),
+	array(
+		array("H", "Hydrogène", 1, 1, "green", "1"),
+		array("He", "Helium", 2, 4, "light_blue", "1")
+	),
+	array(
+		array("Li", "Lithium", 3, 6, "red", "2"),
+		array("Be", "Béryllium", 4, 9, "orange", "2"),
+		array("B", "Bore", 5, 10, "dark_green", "2"),
+		array("C", "Carbone", 6, 12, "green", "2"),
+		array("N", "Azote", 7, 14, "green", "2"),
+		array("O", "Oxygène", 8, 15, "green", "2"),
+		array("F", "Fluor", 9, 18, "yellow",  "2"),
+		array("N", "Néon", 10, 20, "light_blue", "2")
+	),
+	array(
+		array("Na", "Sodium", 11, 22, "red", "3"),
+		array("Mg", "Magnésium", 12, 24, "orange", "3"),
+		array("Al", "Aluminium", 13, 26, "grey", "3"),
+		array("Si", "Silicium", 14, 28, "dark_green", "3"),
+		array("P", "Phosphore", 15, 30, "green", "3"),
+		array("S", "Souffre", 16, 32, "green", "3"),
+		array("Cl", "Chlore", 17, 35, "yellow", "3"),
+		array("Ar", "Argon", 18, 39, "light_blue", "3")
+	),
+	array(
+		array("K", "Potassium", 19, 39, "red", "4"),
+		array("Ca", "Calcium", 20, 40, "orange", "4"),
+		array("Sc", "Scandium", 21, 44, "saumon", "4"),
+		array("Ti", "Titane", 22, 47, "saumon", "4"),
+		array("V", "Vanadium", 23, 51, "saumon", "4"),
+		array("Cr", "Chrome", 24, 52, "saumon", "4"),
+		array("Mn", "Manganèse", 25, 55, "saumon", "4"),
+		array("Fe", "Fer", 26, 56, "saumon", "4"),
+		array("Co", "Cobalt", 27, 59, "saumon", "4"),
+		array("Ni", "Nickel", 28, 58, "saumon", "4"),
+		array("Cu", "Cuivre", 29, 63, "saumon", "4"),
+		array("Zn", "Zinc", 30, 65, "grey", "4"),
+		array("Ga", "Gallium", 31, 69, "grey", "4"),
+		array("Ge", "Germanium", 32, 72, "dark_green", "4"),
+		array("As", "Arsenic", 33, 74, "grey", "4"),
+		array("Se", "Sélénium", 34, 78, "green", "4"),
+		array("Br", "Brome", 35, 79, "yellow", "4")
+	),
+	array(
+		array("Kr", "Krypton", 36, 83, "light_blue", "4"),
+		array("Rb", "Rubidium", 37, 85, "red", "5"),
+		array("Sr", "Strontium", 38, 87, "orange", "5"),
+		array("Y", "Yttrium", 39, 89, "saumon", "5"),
+		array("Zr", "Zirconium", 40, 91, "saumon", "5"),
+		array("Nb", "Niobium", 41, 93, "saumon", "5"),
+		array("Mo", "Molybdène", 42, 96, "saumon", "5"),
+		array("Tc", "Technétium", 43, 98, "saumon", "5"),
+		array("Ru", "Ruthénium", 44, 101, "saumon", "5"),
+		array("Rh", "Rhodium", 45, 103, "saumon", "5"),
+		array("Pd", "Palladium", 46, 106, "saumon", "5"),
+		array("Ag", "Argent", 47, 107, "saumon", "5"),
+		array("Cd", "Cadmium", 48, 112, "grey", "5"),
+		array("In", "Indium", 49, 114, "grey", "5"),
+		array("Sn", "Étain", 50, 118, "grey", "5"),
+		array("Sb", "Antimoine", 51, 121, "dark_green", "5"),
+		array("Te", "Tellure", 52, 127, "dark_green", "5"),
+		array("I", "Iode", 53, 126, "yellow", "5"),
+		array("Xe", "Xénon", 54, 131, "light_blue", "5")
+	),
+	array(
+		array("Cs", "Césium", 55, 133, "red", "6"),
+		array("Ba", "Baryum", 56, 137, "saumon", "6"),
+		array("Lu", "Lutécium", 71, 138, "pink", "6"),
+		array("Hf", "Hafnium", 72, 178, "orange", "6"),
+		array("Ta", "Tantale ", 73, 181, "orange", "6"),
+		array("W", "Tungstène", 74, 184, "orange", "6"),
+		array("Re", "Rhénium", 75, 186, "orange", "6"),
+		array("Os", "Osmium", 76, 190, "orange", "6"),
+		array("Ir", "Iridium", 77, 192, "orange", "6"),
+		array("Pt", "Platine", 78, 195, "orange", "6"),
+		array("Au", "Or", 79, 196, "orange", "6"),
+		array("Hg", "Mercure", 80, 200, "grey", "6"),
+		array("Ti", "Thallium", 81, 204, "grey", "6"),
+		array("Pb", "Plomb", 82, 207, "grey", "6"),
+		array("Bi", "Bismuth", 83, 209, "grey", "6"),
+		array("Po", "Polonium", 84, 209, "grey", "6"),
+		array("At", "Astate", 85, 210, "dark_green", "6"),
+		array("Rn", "Radon", 86, 222, "light_blue", "6")	
+	),
+	array(
+		array("Fr", "Francium", 87, 223, "red", "7"),
+		array("Ra", "Radium", 88, 226, "saumon", "7"),
+		array("Lr", "Lawrencium", 103, 266, "purple", "7"),
+		array("Rf", "Rutherfordium", 104, 267, "orange", "7"),
+		array("Db", "Dubnium", 105, 268, "orange", "7"),
+		array("Sg", "Seaborgium", 106, 271, "orange", "7"),
+		array("Bh", "Bohrium", 107, 272, "orange", "7"),
+		array("Hs", "Hassium", 108, 270, "orange", "7"),
+		array("Mt", "Meitnérium", 109, 276, "white", "7"),
+		array("Ds", "Darmstadtium", 110, 281, "white", "7"),
+		array("Rg", "Roentgenium", 111, 280, "white", "7"),
+		array("Cn", "Copernicium", 112, 285, "orange", "7"),
+		array("Nh", "Nihonium", 113, 286, "white", "7"),
+		array("Fl", "Flérovium", 114, 289, "white", "7"),
+		array("Mc", "Moscovium", 115, 289, "white", "7"),
+		array("Lv", "Livermorium", 116, 293, "white", "7"),
+		array("Ts", "Tennesse", 117, 294, "white", "7"),
+		array("Og", "Oganesson", 118, 294, "white", "7")
+	),
+	array(
+			array("La", "Lanthane", 57, 138, "pink", "8"),
+		array("Ce", "Cérium", 58, 140, "pink", "8"),
+		array("Pr", "Praséodyme", 59, 140, "pink", "8"),
+		array("Nd", "Néodyme", 60, 144, "pink", "8"),
+		array("Pm", "Prométhium", 61, 145, "pink", "8"),
+		array("Sm", "Samarium", 62, 150, "pink", "8"),
+		array("Eu", "Europium", 63, 151, "pink", "8"),
+		array("Gd", "Gadolinium", 64, 157, "pink", "8"),
+		array("Tb", "Terbium", 65, 158, "pink", "8"),
+		array("Dy", "Dysprosium", 66, 162, "pink", "8"),
+		array("Ho", "Holmium", 67, 164, "pink", "8"),
+		array("Er", "Erbium", 68, 167, "pink", "8"),
+		array("Tm", "Thulium", 69, 168, "pink", "8"),
+		array("Yb", "Ytterbium", 70, 173, "pink", "8")
+	),
+	array(
+		array("Ac", "Actinium", 89, 227, "purple", "9"),
+		array("Th", "Thorium", 90, 232, "purple", "9"),
+		array("Pa", "Protactinium", 91, 231, "purple", "9"),
+		array("U", "Uranium", 92, 238, "purple", "9"),
+		array("Np", "Neptunium", 93, 237, "purple", "9"),
+		array("Pu", "Plutonium", 94, 244, "purple", "9"),
+		array("Am", "Américium", 95, 241, "purple", "9"),
+		array("Cm", "Curium", 96, 247, "purple", "9"),
+		array("Bk", "Berkélium", 97, 247, "purple", "9"),
+		array("Cf", "Californium", 98, 251, "purple", "9"),
+		array("Es", "Einsteinium", 99, 252, "purple", "9"),
+		array("Fm", "Fermium", 100, 257, "purple", "9"),
+		array("Md", "Mendélévium", 101, 258, "purple", "9"),
+		array("No", "Nobélium", 102, 259, "purple", "9")
+	)
 );
+
+function read_array($array)
+{
+	$i = 0;
+	while (isset($array[$i]))
+	{
+		if (is_array($array[$i][0]))
+			read_array($array[$i]);
+		else
+		{
+			print_r($array[$i]);
+			/*
+			echo "<pre>
+			Symbole : " . $array[$i][0] . "<br />
+			Noms : " . $array[$i][1] . "<br />
+			Num Atomique : " . $array[$i][2] . "<br />
+			Masse Atomique : " . $array[$i][3] . "<br />
+			Couleur : " . $array[$i][4] . "<br />
+			</pre>";
+			*/
+		}
+		$i++;
+	}
+	echo "<pre>NEW LINE</pre>";
+}
+
+read_array($array);

fedcfa9d6beb6161ce247d64e07c737dd758ff51
Carnagiul-42 <piquerue@student.42.fr>
2018-05-28 13:40:42 +0200

    Update Connection Page

--- CommonTools/Class/Page/Connection.Page.class.php
+++ CommonTools/Class/Page/Connection.Page.class.php
@@ -15,60 +15,60 @@ class Connection implements PageInterface
 		return ;
 	}
 
-    public function display()
-    {
-        global $tpl;
+	public function display()
+	{
+		global $tpl;
 
-        if (isset($_POST))
-            $this->form_post();
-        if (isset($_GET))
-            $this->form_get();
-        if (isset($_SESSION["user"]))
-            $tpl->setFileName("connection_log");
-        else
-            $tpl->setFileName("connection");
-    }
+		if (isset($_POST))
+			$this->form_post();
+		if (isset($_GET))
+			$this->form_get();
+		if (isset($_SESSION["user"]))
+			$tpl->setFileName("connection_log");
+		else
+			$tpl->setFileName("connection");
+	}
 
-    public function form_post()
-    {
-        global $user;
-        global $lang;
+	public function form_post()
+	{
+		global $user;
+		global $lang;
 
-        if ($_POST["action"] == "connect")
-        {
-            $pass = isset($_POST['pass']) ? $_POST['pass'] : "";
-            $name = isset($_POST['name']) ? $_POST['name'] : "";
-            $ret = $user->connect($name, $pass);
-            echo $lang->getLang($ret, 'FR');
-        }
-        if ($_POST["action"] == "disconnect")
-        {
-            $ret = $user->disconnect();
-            echo $lang->getLang($ret, 'FR');
-        }
-    }
+		if ($_POST["action"] == "connect")
+		{
+			$pass = isset($_POST['pass']) ? $_POST['pass'] : "";
+			$name = isset($_POST['name']) ? $_POST['name'] : "";
+			$ret = $user->connect($name, $pass);
+			echo $lang->getLang($ret, 'FR');
+		}
+		if ($_POST["action"] == "disconnect")
+		{
+			$ret = $user->disconnect();
+			echo $lang->getLang($ret, 'FR');
+		}
+	}
 
-    public function form_get()
-    {
-        global $user;
-        global $lang;
+	public function form_get()
+	{
+		global $user;
+		global $lang;
 
-        if ($_GET["action"] == "disconnect")
-        {
-            $ret = $user->disconnect();
-            echo $lang->getLang($ret, 'FR');
-        }
-    }
+		if ($_GET["action"] == "disconnect")
+		{
+			$ret = $user->disconnect();
+			echo $lang->getLang($ret, 'FR');
+		}
+	}
 
-    public function ajax_get()
-    {
+	public function ajax_get()
+	{
 
-    }
+	}
 
-    public function ajax_php()
-    {
+	public function ajax_php()
+	{
 
-    }
+	}
 
 	public function toggle_verbrose()
 	{
@@ -80,10 +80,10 @@ class Connection implements PageInterface
 		return ($this->verbose);
 	}
 
-    public function doc()
-    {
-    	return (file_get_contents('docs/Connection.doc.txt'));
-    }
+	public function doc()
+	{
+		return (file_get_contents('docs/Connection.doc.txt'));
+	}
 }
 
 ?>

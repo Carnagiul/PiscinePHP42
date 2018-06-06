18908006cdb34bcc9bc8918b97c261d7802350fe
Carnagiul-42 <piquerue@student.42.fr>
2018-06-06 19:02:41 +0200

    update project

---
+++ Day 06/ex04/Camera.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piquerue
+ * Date: 6/6/18
+ * Time: 6:58 PM
+ */
+
+require_once ("Color.class.php");
+require_once ("Vertex.class.php");
+require_once ("Vector.class.php");
+require_once ("Matrix.class.php");
+
+class Camera
+{
+    private $width;
+    private $height;
+    private $near;
+    private $far;
+    private $fov;
+    private $ratio;
+
+    function __construct($data)
+    {
+
+    }
+}
